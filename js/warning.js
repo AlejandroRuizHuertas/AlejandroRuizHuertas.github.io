0270ffc230fa23e57ca40bb6edd3a8328a2df640
Alejandro Ruiz Huertas <peditito@gmail.com>
2022-03-04 19:11:22 +0100

    Actualizado con datos del máster

---
+++ js/warning.js
@@ -0,0 +1,4 @@
+console.log("¡Eh! ¡Qué haces mirando el código! Si estás aquí, quiero darte la bienvenida "+ 
+"a los entresijos de mi página web, has encontrado el easter egg. Eso significa que eres "+
+"una persona de bien, por lo que te invito a pasarte por mi LinkedIn a mandarme una captura "+
+"de este mensaje :)")
